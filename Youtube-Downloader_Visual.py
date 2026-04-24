4e86f753709634b85a802cac11334f8d8edf531f
Gabriela Regina de Souza <122756066+GabrielaSouzaDev@users.noreply.github.com>
2026-04-23 21:49:13 -0300

    aula 23/04

--- Youtube-Downloader_Visual.py
+++ Youtube-Downloader_Visual.py
@@ -7,8 +7,67 @@ ctk.set_default_color_theme('dark-blue')
 
 # criação da janela
 janela = ctk.CTk()
-janela.geometry('900x700')
+janela.geometry('600x500')
 janela.title('Downloader Youtube')
+
+# icone da janela
+janela.iconbitmap('icone.ico')
+
+#função para barra de progresso
+# def progresso(stream, chunk, bytes_remaining):
+
+
+
+
+# função para baixar
+def baixar():
+    # captura o link digitado dentro do input
+    url = link.get()
+    # essa variavel abre toda a biblioteca e atrela todas as configurações de um video dentro do video desse link
+    yt = YouTube(url)
+    # baixar resolução padrão
+    yt.streams.first().download()
+    status.configure(text='Download concluído')
+
+
+
+# elementos da tela
+# label titulo
+titulo = ctk.CTkLabel(janela, 
+                      text='Downloader Youtube',
+                      font=('Arial', 30),
+                      text_color='red'
+                      )
+titulo.pack(pady=80)
+
+# entrada do link
+link = ctk.CTkEntry(janela, 
+                    placeholder_text='Cole seu link aqui...',
+                    width=260,
+                    height=36,
+                    font=('Arial', 15))
+link.pack(pady=30)
+
+# botão 
+donwload = ctk.CTkButton(janela, 
+                         text='Download',
+                         command= baixar,
+                         corner_radius= 15,
+                         hover_color=('red'),
+                         font=('Arial', 15))
+donwload.pack(pady=30)
+
+# barra de progresso
+barra_progresso = ctk.CTkProgressBar(janela,
+                                     orientation='horizontal')
+barra_progresso.pack(pady=30)
+
+# status do download
+status = ctk.CTkLabel(janela, 
+                      text='',
+                      font=('Arial', 15))
+status.pack(pady=30)
+
 
 # para gerar a janela
 janela.mainloop()
--- pp_aula01_ex02_API02.py
+++ pp_aula01_ex02_API02.py
@@ -8,18 +8,30 @@ url_cep = f'https://viacep.com.br/ws/{cep}/json/'
 resposta_cep = requests.get(url_cep)
 dados_cep = resposta_cep.json()
 
-cidade = resposta_cep["localidade"]
-url_localidade = f'https://geocoding-api.open-meteo.com/v1/search'
+# Tratamento de erros
+if "erro" in dados_cep:
+    print('CEP não encontrado!')
+else:
+    cidade = dados_cep["localidade"]
+    print(f'Cidade encontrada: {cidade}')
 
-# API Temperatura
-latitude = resposta_cep
-longitude = resposta_cep
+# API Geocoding (trazendo latitude e longitude da cidade)
+url_localidade = f'https://open-meteo.com{cidade}&count=1&language=pt'
+resposta_localidade = requests.get(url_localidade)
+dados_localidade = resposta_localidade.json()
 
-url_clima = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
+if "resultado" in dados_localidade:
+    latitude = dados_localidade["results"][0]["latitude"]
+    longitude = dados_localidade["results"][0]["longitude"]
 
-resposta = requests.get(url_clima)
-dados = resposta.json()
+    # API Temperatura
+    url_clima = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
 
-print(f'Temperatura: {dados["current"]["temperature_2m"]}')
-print()
-print(f'Velocidade do vento: {dados["current"]["wind_speed_10m"]}')
+    resposta_clima = requests.get(url_clima)
+    dados_clima = resposta.json()
+
+    print(f'Condições em {cidade}:')
+    print(f'Temperatura: {dados_clima["current"]["temperature_2m"]}°C')
+    print(f'Velocidade do vento: {dados_clima["current"]["wind_speed_10m"]}')
+else:
+    print("Coordenadas da cidade não encontradas.")
